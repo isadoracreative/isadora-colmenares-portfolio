1b8e3847bf75ca72dc31c6706eccd11e95db3ba6
Isadora <10870647+isadoracreative@users.noreply.github.com>
2025-02-02 13:36:28 -0500

    reorganize partials and style navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div className="App container-fluid px-0">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Isadora Colmenares</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg pl-4 pr-3">
+        <a className="navbar-brand text-dark" href="#">Isadora Colmenares</a>
+        <button className="navbar-toggler border-0 p" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span><i class="fa-sharp-duotone fa-regular fa-bars"></i></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
